aeb1bde429e17335fdf79a3e8f5759dde02a8a79
MihaiB94 <bormihverale@gmail.com>
2022-02-02 15:06:34 +0100

    navbar changes

--- script/main.js
+++ script/main.js
@@ -68,7 +68,7 @@ window.onscroll = function () {
 
   if (prevScrollpos > currentScrollPos) {
     navbar.style.top = '0';
-    if (window.pageYOffset > '20') {
+    if (window.pageYOffset > '100') {
       navbar.style.boxShadow =
         'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px';
       navbar.style.opacity = '0.98';
