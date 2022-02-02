b5fe016f88b0bd0a40026d03f6fedd8cedf8303a
MihaiB94 <bormihverale@gmail.com>
2022-02-02 16:48:39 +0100

    nav

--- script/main.js
+++ script/main.js
@@ -77,10 +77,11 @@ window.onscroll = function () {
     } else {
       navbar.style.boxShadow = 'none';
       logo.style.padding = '1.5rem 0';
+      navbar.style.opacity = '1';
       logo.style.transition = 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)';
     }
   } else {
-    navbar.style.opacity = '0';
+    navbar.style.top = '-100px';
   }
 
   //Active Section
